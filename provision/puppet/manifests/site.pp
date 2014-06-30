da4cd26fd8db87e99e873d9a497d045f066e7484
Luis Mayta <slovacus@gmail.com>
2014-06-30 11:43:27 -0500

    fixed modules initialized

--- provision/puppet/manifests/site.pp
+++ provision/puppet/manifests/site.pp
@@ -7,16 +7,8 @@ Exec
   path => ["/usr/bin", "/bin", "/usr/sbin", "/sbin", "/usr/local/bin", "/usr/local/sbin"]
 }
 
-exec 
-{ 
-    'apt-get update':
-        command => '/usr/bin/apt-get update',
-}
-
 include bootstrap
 include apache
 include php
-include other #curl and sqlite
 include mysql
 include vim
-include git
